45d41f0b71a0be10f6fde5770656023498856e25
Sanjaee <afrizaahmad18@gmail.com>
2024-10-03 20:27:01 +0700

    add react-router react-router-dom, navbar

---
+++ UI-APBD/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center fixed top-0 w-full z-30 px-20 h-28 border'>
+      <img className='w-32' src="https://data.go.id/assets/image-8@2x.png" alt="logo" />
+      <div>
+  
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
